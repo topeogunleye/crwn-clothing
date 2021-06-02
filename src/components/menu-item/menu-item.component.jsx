8f00dd2c907787181673abfe7847b56107af97f2
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-02 11:10:34 +0100

    adds directory component and directory styles to project

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,10 @@
 import './menu-item.styles.scss';
 
-const MenuItem = () => {
+const MenuItem = ({ title }) => {
   return (
     <div className="menu-item">
       <div className="content">
-        <h1 className="title">HATS</h1>
+        <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
