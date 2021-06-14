b3bd165297ca2f891ea190b943c0f243ca8ce641
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-14 10:17:59 +0100

    Adds crown svg log and imports it into header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import './header.styles.scss'
 import { Link } from 'react-router-dom'
+import { ReactComponent as logo } from '../../assets/crown.svg'
 
-const Header = () => {
+const Header = () => (
   <div className="header">
+    <Link to="/">
 
+    </Link>
   </div>
-}
+)
 
 export default Header;
