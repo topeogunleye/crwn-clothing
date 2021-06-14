3fa05723ad3c28c944632288ce8874a547c2a79a
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-14 08:29:16 +0100

    Adds JSX to cart item component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,1 +1,10 @@
 import './card-icon.styles.scss'
+
+const CartItem = ({ item: { imageUrl, price, name}}) => (
+  <div className="cart-item">
+  <img src={imageUrl} alt="item" />
+  <div className="item-details">
+    
+  </div>
+  </div>
+)
