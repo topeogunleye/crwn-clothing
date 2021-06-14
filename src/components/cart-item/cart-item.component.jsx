0bc80dcc863ad22ce258edae301ff2a683bc1b0a
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-14 08:34:11 +0100

    Adds JSX to cart item component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -4,7 +4,8 @@ const CartItem = ({ item: { imageUrl, price, name}}) => (
   <div className="cart-item">
   <img src={imageUrl} alt="item" />
   <div className="item-details">
-    
+    <span className="name">{name}</span>
+    <span className="price">${price}</span>
   </div>
   </div>
 )
