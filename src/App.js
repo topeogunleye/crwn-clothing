3cf1935803baabdbca105c5cdca9d40b995e83a8
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-14 10:27:27 +0100

    Places Header outside of the Swith and Route that contains all of our page components

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from './components/header/header.component.jsx'
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
