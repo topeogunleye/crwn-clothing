34e84a42c84dd42c6e2c706e16f2a9bb840d6819
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-15 07:38:31 +0100

    Adds handle submit function to sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -13,13 +13,19 @@ class SignIn extends Component {
     }
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({ email: '', password: ''})
+  }
+
   render() {
     return(
       <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
         <input name="email" type="email" value={this.state.email} required />
         <label>Email</label>
         <input name="password" type="password" value={this.state.email} required />
