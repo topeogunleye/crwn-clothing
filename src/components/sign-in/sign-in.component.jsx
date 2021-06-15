c9b07952aa9a0ce36cb9bc4fb28cce1d91ecf8a5
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-15 07:44:53 +0100

    Adds handle change function to sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -19,6 +19,12 @@ class SignIn extends Component {
     this.setState({ email: '', password: ''})
   }
 
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value })
+  }
+
   render() {
     return(
       <div className="sign-in">
@@ -28,7 +34,7 @@ class SignIn extends Component {
         <form onSubmit={this.handleSubmit}>
         <input name="email" type="email" value={this.state.email} required />
         <label>Email</label>
-        <input name="password" type="password" value={this.state.email} required />
+        <input name="password" type="password" value={this.state.email} onChange={this.handleChange} required />
         <label>Password</label>
 
         <input type="submit" value="Submit Form"/>
